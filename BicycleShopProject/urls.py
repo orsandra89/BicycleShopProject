f0c540b90645f6c062095221d596ece56196aff5
eugeniisenyush <senusheugen@gmail.com>
2024-04-14 12:56:15 +0200

    add item to order management

--- BicycleShopProject/urls.py
+++ BicycleShopProject/urls.py
@@ -20,9 +20,12 @@ from BicycleShopProject.view import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('register/', views.register, name='register'),
+    # path('login/', views.user_login, name='login'),
     path('bicycles/', views.get_bicycle_list, name='bicycle_list'),
     path('bicycles/<int:bicycle_id>/', views.get_bicycle_by_id, name='bicycle_detail'),
     path('customer/<int:customer_id>/orders', views.get_order_list_for_customer, name='order_list'),
     path('customer/<int:customer_id>/orders/<int:order_id>/', views.customer_order_ids, name='user_order_ids'),
-    path('customer/<int:customer_id>/create_order/', views.create_order, name='create_order')
+    path('customer/<int:customer_id>/create_order/', views.create_order, name='create_order'),
+    path('orders/<int:order_id>/add-item/<int:item_id>', views.add_item_to_order, name='add_item_to_order'),
 ]
--- BicycleShopProject/view/views.py
+++ BicycleShopProject/view/views.py
@@ -1,10 +1,12 @@
 import json
 
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.middleware.csrf import get_token
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
 
-from BicycleShopProject.models.models import Product, Order, Customer
+from BicycleShopProject.models.models import Product, Order, Customer, OrderItem
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET, require_POST
 
@@ -89,11 +91,8 @@ def create_order(request, customer_id):
     try:
         customer = get_object_or_404(Customer, customer_id=customer_id)
         data = json.loads(request.body)
-
         # Retrieve fields from the parsed data
         order_id = data.get('order_id')
-        # customer_id = data.get('customer_id')
-        # order_id = request.POST.get('order_id')
         state = Order.DRAFT
         print(order_id)
         print(customer)
@@ -112,3 +111,63 @@ def create_order(request, customer_id):
         return JsonResponse({'order_id': order.order_id}, status=201)
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=400)
+
+@csrf_exempt
+@require_POST
+def add_item_to_order(request, order_id):
+    try:
+        order = get_object_or_404(Order, order_id=order_id)
+        data = json.loads(request.body)
+
+        # Extract fields from the request data
+        product_id = data.get('product_id')
+        quantity = data.get('quantity')
+
+        # Validate required fields
+        if not all([product_id, quantity]):
+            return JsonResponse({'error': 'Product ID and Quantity are required'}, status=400)
+
+        # Retrieve the product
+        product = get_object_or_404(Product, pk=product_id)
+
+        # Create the order item
+        order_item = OrderItem.objects.create(
+            order=order,
+            product=product,
+            quantity=quantity,
+            list_price=product.price,
+            discount=0
+        )
+
+        return JsonResponse({'order_item_id': order_item.id}, status=201)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    except Product.DoesNotExist:
+        return JsonResponse({'error': 'Product does not exist'}, status=400)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+# @require_POST
+# def register(request):
+#     form = UserCreationForm(request.POST)
+#     if form.is_valid():
+#         form.save(commit=False)
+#         form.cleaned_data['username'] = form.cleaned_data['email']  # Use email as username
+#         form.save()
+#         username = form.cleaned_data.get('email')
+#         raw_password = form.cleaned_data.get('password1')
+#         user = authenticate(username=username, password=raw_password)
+#         login(request, user)
+#         return redirect('home')  # Redirect to the desired page after registration
+#     else:
+#         return JsonResponse({'error': 'Invalid form data'}, status=400)
+#
+# @require_POST
+# def user_login(request):
+#     form = AuthenticationForm(request, request.POST)
+#     if form.is_valid():
+#         user = form.get_user()
+#         login(request, user)
+#         return redirect('home')  # Redirect to the desired page after login
+#     else:
+#         return JsonResponse({'error': 'Invalid credentials'}, status=400)
